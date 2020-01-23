da29fb7e6bf926dedb553677a2c8693c6384bb55
paivo <paveee777@gmail.com>
2020-01-23 17:49:39 +0200

    Tehtiin 1.3 ja 1.4 jäi kesken

--- osa1/1.1_sovellus/src/index.js
+++ osa1/1.1_sovellus/src/index.js
@@ -12,20 +12,24 @@ const Header = (props) => {
 }
 
 const Content = (props) => {
+  console.log(props)
     return (
+      
       <div>
-        <Part part={props.part1} exercises={props.exercises1} />
-        <Part part={props.part2} exercises={props.exercises2} />
-        <Part part={props.part3} exercises={props.exercises3} />
+        <Part part={props[1]} />
+        <Part part={props[1]} />
+        <Part part={props[1]} />
       </div>
     )
 }
 
 const Part = (props) => {
+    const fieldName = 'name' 
     return (
       <div>
         <p>
-          {props.part} {props.exercises}
+          
+          {props.part[fieldName]} {props.part[1]}
         </p>
       </div>
     )
@@ -43,18 +47,27 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} exercises1={exercises1}  part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
