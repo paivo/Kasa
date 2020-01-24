6b3d4341b6fe81d7ff011abf6f6b7bf194e2b5ab
paivo <paveee777@gmail.com>
2020-01-24 12:45:56 +0200

    tehtiin tehtävä 1.4

--- osa1/1.1_sovellus/src/index.js
+++ osa1/1.1_sovellus/src/index.js
@@ -13,23 +13,27 @@ const Header = (props) => {
 
 const Content = (props) => {
   console.log(props)
+  
     return (
       
       <div>
-        <Part part={props[1]} />
-        <Part part={props[1]} />
-        <Part part={props[1]} />
+        <Part part={props.parts[0]} />
+        <Part part={props.parts[1]} />
+        <Part part={props.parts[2]} />
       </div>
     )
 }
 
 const Part = (props) => {
+   
+    console.log(props)
+    
     const fieldName = 'name' 
     return (
       <div>
         <p>
           
-          {props.part[fieldName]} {props.part[1]}
+          {props.part["name"]} {props.part.exercises}
         </p>
       </div>
     )
@@ -39,7 +43,7 @@ const Total = (props) => {
     return (
       <div>
         <p>
-        Number of exercises {props.total}
+        Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
         </p>
       </div>
     )
