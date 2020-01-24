f58103e2ba732ad39d37cba6f14016ce9ea3e6e7
paivo <paveee777@gmail.com>
2020-01-24 15:10:02 +0200

    tehtiin osa 1 c

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,27 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Hello = (props) => {
-    return (
-      <div>
-        <p>
-          Hello {props.name}, you are {props.age} years old
-        </p>
-      </div>
-    )
-  }
-  
-  const App = () => {
-    const nimi = 'Pekka'
-    const ika = 10
-  
-    return (
-      <div>
-        <h1>Greetings</h1>
-        <Hello name="Toppe" age={1 + 10} />
-        <Hello name={nimi} age={ika} />
-      </div>
-    )
-  }
-  
-  ReactDOM.render(<App />, document.getElementById('root'))
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const App = (props) => {
+  const [ counter, setCounter ] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
+
+  return (
+    <div>
+      <Display counter={counter}/>
+      <Button
+        handleClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        handleClick={setToZero}
+        text='zero'
+      />     
+      <Button
+        handleClick={decreaseByOne}
+        text='minus'
+      />           
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+)
