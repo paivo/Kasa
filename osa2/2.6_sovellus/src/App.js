4d80cff522a800c3963b92a58d311757244a840a
paivo <paveee777@gmail.com>
2020-01-31 16:09:56 +0200

    aloitettiin 2.11, seuraavaksi täytyy lisätä personeille id:t

--- osa2/2.6_sovellus/src/App.js
+++ osa2/2.6_sovellus/src/App.js
@@ -1,16 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Person from './components/Person'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [newFilter, setNewFilter] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+  console.log('render', persons.length, 'persons')
 
   const addPerson = (event) => {
     var names = persons.map((person) => person.name)
@@ -52,7 +59,7 @@ const App = () => {
       <h2>add a new</h2>
       <AddPersonForm addPerson={addPerson} newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange} />
       <h2>Numbers</h2>
-        <ShowNumbers personsToShow={personsToShow}/>
+      <ShowNumbers personsToShow={personsToShow}/>
     </div>
   )
 
